157f8fbb3e181220cea161377ea90a40d39be489
abdeladim20 <linuxacount1@gmail.com>
2025-07-29 08:55:22 +0100

    done

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -1,28 +1,41 @@
 pub fn capitalize_first(input: &str) -> String {
-    let mut result = input.chars().collect::<Vec<char>>();
+    if input.is_empty() {
+        return String::new();
+    }
+
+    let mut result: Vec<char> = input.chars().collect();
     result[0].make_ascii_uppercase();
-    result.iter().collect::<String>()
+    result.into_iter().collect()
 }
 
 pub fn title_case(input: &str) -> String {
-    let mut result = String::new(); 
-    for val in input.split_whitespace() {
-        result.push_str(&(capitalize_first(val) + " "));
-    }
+    let mut result = String::new();
+    let mut capitalize = true;
 
-    result.trim().to_string()
+    for c in input.chars() {
+        if c.is_whitespace() {
+            result.push(c);
+            capitalize = true;
+        } else if capitalize {
+            result.push(c.to_ascii_uppercase());
+            capitalize = false;
+        } else {
+            result.push(c);
+        }
+    }
+    result
 }
 
 pub fn change_case(input: &str) -> String {
     let mut result = String::new();
     for c in input.chars() {
-        if c.is_lowercase() {
-            result.push(c.to_ascii_uppercase());
-        }else if c.is_uppercase() {
+        if c.is_uppercase() {
             result.push(c.to_ascii_lowercase());
-        }else {
+        } else if c.is_lowercase() {
+            result.push(c.to_ascii_uppercase());
+        } else {
             result.push(c);
-        };
+        }
     }
     result
 }
