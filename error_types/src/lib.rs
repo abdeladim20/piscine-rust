71344dc9a1ffd08331120ca387598f7e25b836f2
abdeladim20 <linuxacount1@gmail.com>
2025-08-02 10:08:37 +0100

    leap year

---
+++ error_types/src/lib.rs
@@ -0,0 +1,32 @@
+// this will be the structure that wil handle the errors
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    // expected public fields
+    pub 
+}
+
+impl FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        todo!()
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    // expected public fields
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        todo!()
+    }
+}
+
+/* 
+Form { name: "Lee", password: "qwqwsa1dty_" }
+Ok(())
+Err(FormError { form_values: ("name", ""), date: "2022-10-17 12:09:25", err: "Username is empty" })
+Err(FormError { form_values: ("password", "dty_1"), date: "2022-10-17 12:09:25", err: "Password should be at least 8 characters long" })
+Err(FormError { form_values: ("password", "asdasASd(_"), date: "2022-10-17 12:09:25", err: "Password should be a combination of ASCII numbers, letters and symbols" })
+Err(FormError { form_values: ("password", "asdasASd123SA"), date: "2022-10-17 12:09:25", err: "Password should be a combination of ASCII numbers, letters and symbols" })
+*/
