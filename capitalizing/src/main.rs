34a529075586ee1c7ab48ba25769fd112d773298
abdeladim20 <linuxacount1@gmail.com>
2025-08-02 14:36:59 +0100

    add main for test

---
+++ capitalizing/src/main.rs
@@ -0,0 +1,13 @@
+use capitalizing::*;
+
+fn main() {
+    println!("{}", capitalize_first("joe is missing"));
+    println!("{}", title_case("jill is  leaving A"));
+    println!("{}", change_case("heLLo THere"));
+}
+
+/* 
+Joe is missing
+Jill Is Leaving A
+HEllO thERE
+*/
