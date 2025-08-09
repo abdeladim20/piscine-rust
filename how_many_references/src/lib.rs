0c7ab21ada2687b16480887dc4eb66c016839a2f
ajabbour <abdeladimjabbouri0@gmail.com>
2025-08-09 10:59:33 +0100

    shared ownership

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,25 @@
+pub use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Self {
+        Self {
+            ref_list: ref_list,
+        }
+    }
+
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        self.ref_list.retain(|value| !Rc::ptr_eq(&element, value));
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+    Rc::strong_count(ref_list)
+}
