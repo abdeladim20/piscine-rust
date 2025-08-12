6a504985cb10e5ffb5733eda112a848a2c827859
ajabbour <abdeladimjabbouri0@gmail.com>
2025-08-12 08:49:00 +0100

    done

---
+++ roman_numbers/src/lib.rs
@@ -0,0 +1,65 @@
+use crate::RomanDigit::*;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RomanDigit {
+	Nulla,
+	I,
+	V,
+	X,
+	L,
+	C,
+	D,
+	M,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RomanNumber(pub Vec<RomanDigit>);
+
+impl From<u32> for RomanDigit {
+    fn from(n: u32) -> Self {
+        match n {
+            1 => I,
+            5 => V,
+            10 => X,
+            50 => L,
+            100 => C,
+            500 => D,
+            1000 => M,
+            _ => Nulla,
+        }
+    }
+}
+
+impl From<u32> for RomanNumber {
+    fn from(n: u32) -> Self {
+        if n == 0 {
+            return Self(vec![Nulla]);
+        }
+
+        let roman_number = vec![
+            (1000, vec![M]),
+            (900, vec![C, M]),
+            (500, vec![D]),
+            (400, vec![C, D]),
+            (100, vec![C]),
+            (90, vec![X, L]),
+            (50, vec![L]),
+            (40, vec![X, L]),
+            (10, vec![X]),
+            (9, vec![I, X]),
+            (5, vec![V]),
+            (4, vec![I, V]),
+            (1, vec![I]),
+        ];
+
+        let mut num = n;
+        let mut result_digits = Vec::new();
+        for (value, digits) in &roman_number {
+            while num >= *value {
+                result_digits.extend_from_slice(digits);
+                num -= *value;
+            }
+        }
+        Self(result_digits)
+    }
+}
